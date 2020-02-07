c01c4383fa7ecce847939d8caf773a66ede63df3
Nicolaas Nel <nicon@infoslips.com>
2020-02-07 13:48:44 +0200

    Initial commit.

---
+++ marked.js
@@ -0,0 +1,56 @@
+let mark = `
+# Top Employer
+DHL has been certified as a Top Employer for the past five years, since 2015, by The Top Employer Institute. DHL is certified in 21 African countries, Sub Sahara Africa and Globally as well as The Intercontinental Award. DHL can proudly call itself one of the world’s leading employers. 
+
+The awards require DHL to complete the HR Best Practices Survey: a comprehensive analysis of the human resources (HR) environment. This research critically assesses HR management and employee conditions within the organization covering the areas of Onboarding, Culture, Workplace Planning, Career  & Succession Management, Compensation & benefits, HR Analytics, Leadership Development, Performance Management and Talent Acquisition. Only employers that achieve the required level will qualify for certification.
+
+![alt text](images/pic1.png "Pic 1") ![alt text](images/pic2.png "Pic 2")
+
+# Great place to Work For
+It is with great pride that in 2018, DHL has been certified in 29 countries. DHL has been ranked 6th Best Workplaces in the World. In 2017 DHL was 8th.
+
+More than 6,600 organizations participated in the survey process, representing the voices of 10 million employees worldwide. In order to qualify DHL was named in a minimum of 5 national Best Workplaces lists within 58 countries, having a staff compliment of over 5,000 worldwide, and had at least 40% of the company's workforce based outside of its home country
+
+We are consistently growing by the number of countries certified by Great Place to Work®. Year by year we ensure better ranking at Country, Regional and Global level. The Great Place to Work® certifications show possible candidates that we have a high-trust workplace culture.
+
+* Logistic Company (DHL)
+
+Just a few months after Neil Armstrong took the first step on the moon, Adrian Dalsey, Larry Hillblom and Robert Lynn in San Francisco took a step destined to alter the global economy: In 1969, they founded the company DHL - the three letters stand for the initials of their last names.
+
+Their new business idea formed the cornerstone for a globally operating enterprise. At first, the company founders personally transported cargo documents from San Francisco to Honolulu by plane. This made it possible to begin customs processing of a ship's cargo before the actual arrival of the shipment, thereby significantly reducing waiting times in the harbour.
+
+DHL became a wholly owned subsidiary of Deutsche Post in 2002. The Group is organized into four operating divisions: Post - eCommerce - Parcel, Express, Global Forwarding, Freight, and Supply Chain.
+
+* DHL Brand
+
+# Brand
+Deutsche Post DHL Group brings two powerful brands to the marketplace.
+1. The Deutsche Post brand stands for personal proximity, reliable quality and ground-breaking services. 
+
+2. The DHL brand stands for personal commitment, proactive solutions and local strength in the parcel business as well as in international express, logistics and mail operations. 
+
+The Group brings together these two brands under one roof. DHL was rated no. 62 in a ranking of the world’s top 100 most valuable brands, with a brand value of US$20.6 billion. Deutsche Post is ranked 21st in the German top 50, with a brand value of US$3.6 billion.
+
+We are ‘the world’s most international company.’ Our DHL brand is a recognized name and we are one of the best employers in the world.
+
+![alt text](images/pic3.png "Pic 3")
+
+# Sponsorships
+DHL has actively sponsored in areas of sport, education, arts & culture. Some of DHL sponsorships include but not limited to:
+
+* DHL-coloured Team Penske Porche RS Spyder LMP2 racing cars of Penske Racing
+* Manchester United Football Club
+* FC Bayern Munich
+* South African Western Cape Rugby Union
+* 2018 Golden Globe Race
+* Cirque du Soleil
+* IMG Fashion
+* Surf Life Australia
+* MotoGP
+* Formula 1
+* The Grand Tour Show
+* Ludwig van Beethoven mobile exhibition "BTHVN on Tour"
+
+![alt text](images/pic4.png "Pic 4")![alt text](images/pic5.png "Pic 5")
+
+`;
